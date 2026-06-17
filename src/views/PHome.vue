6f1f4c30bb4e8b1b854593ae3ed5aea9f599c7eb
Sharofjon <sharofjon@192.168.0.113>
2026-06-17 10:19:26 +0500

    creating CBannerCard

--- src/views/PHome.vue
+++ src/views/PHome.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1 class="text-red-500">Home Page</h1>
+   <div class="bg-[url('https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg')]">    
+     <h1 class="text-red-500">Home Page</h1>
+    <CBannerCard/>
+   </div>
 </template>
+<script setup lang="ts">
+import CBannerCard from '@/components/Cards/CBannerCard.vue';
+</script>
