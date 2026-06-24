5e197b54c27bcd46e355f0cce8fc10ab474fb61c
Mironshoh900 <mironshohd7@gmail.com>
2026-06-24 12:28:45 +0500

    created CAbout

--- src/views/PHome.vue
+++ src/views/PHome.vue
@@ -1,8 +1,11 @@
 <template>
    <div class="bg-gray-900">
      <CBanner/>
+     <CAbout/>
+      
    </div>
 </template>
 <script setup lang="ts">
 import CBanner from '@/components/Sections/CBanner.vue'
+import CAbout from '@/components/Sections/CAbout.vue'
 </script>
