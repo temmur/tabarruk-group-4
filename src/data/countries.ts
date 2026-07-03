01697977d88ecfb1c376a48feedb76ffb903507d
temmur <143727496+temmur@users.noreply.github.com>
2026-07-03 10:46:34 +0500

    fix issues

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -3,42 +3,49 @@ export const countries = [
     name: "O'zbekiston",
     flag: '/images/dropdown-flag-icons/Uzbekistan.svg',  
     count: 175, 
-    route: '/countries/uzbekistan'
+    route: 'uzbekistan',
+    id: 1
  },
   { 
     name: 'Turkiya',     
     flag: '/images/dropdown-flag-icons/Turkiye.svg',      
     count: 150, 
-    route: '/countries/turkey'
+    route: 'turkey',
+    id: 2
  },
   { 
     name: 'Turkmaniston',
     flag: '/images/dropdown-flag-icons/Turkmenistan.svg', 
     count: 20,  
-    route: '/countries/turkmenistan'
+    route: 'turkmenistan',
+    id: 3
  },
   { 
     name: 'Ozarbayjon',  
     flag: '/images/dropdown-flag-icons/Azerbaijan.svg',   
     count: 0,   
-    route: '/countries/azerbaijan'
+    route: 'azerbaijan',
+    id: 4
  },
   { 
     name: "Qozog'iston", 
     flag: '/images/dropdown-flag-icons/Kazakhstan.svg',   
     count: 51,  
-    route: '/countries/kazakhstan' 
+    route: 'kazakhstan',
+    id: 5
 },
   { 
     name: "Qirg'iziston",
     flag: '/images/dropdown-flag-icons/Kyrgyzstan.svg',   
     count: 19,  
-    route: '/countries/kyrgyzstan'
+    route: 'kyrgyzstan',
+    id: 6
  },
   { 
     name: 'Vengriya',    
     flag: '/images/dropdown-flag-icons/Hungary.svg',      
     count: 14,  
-    route: '/countries/hungary'
+    route: 'hungary',
+    id: 7
  },
 ]
